50962079c1b70fc4bbadb239d7d62788bc6b3b87
FaraJoenaja <lexzaai92@gmail.com>
2025-04-17 20:32:39 +0700

    Create post_response.sh

---
+++ backend/post_response.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+user="anonymous"
+question=$(echo "$1" | tr -d '\n' | tr -d '\r')
+answer=$(echo "$2" | tr -d '\n' | tr -d '\r')
+
+curl -X POST -H "Content-Type: application/json" \
+  -d "{\"user\":\"$user\",\"question\":\"$question\",\"answer\":\"$answer\"}" \
+  'https://script.google.com/macros/s/AKfycbyaqTWPS4Whky_66EjweC8PXGyjj78DvTqLRlZqRTmrTJAKLOvxGotKaHFLpZAwvXJs/exec'
