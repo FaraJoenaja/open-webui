36b28ab062748ecb99d78853b51da5eddc1215d1
FaraJoenaja <lexzaai92@gmail.com>
2025-04-18 10:56:15 +0700

    Update post_response.sh

--- backend/post_response.sh
+++ backend/post_response.sh
@@ -4,5 +4,5 @@ question=$(echo "$1" | tr -d '\n' | tr -d '\r')
 answer=$(echo "$2" | tr -d '\n' | tr -d '\r')
 
 curl -X POST -H "Content-Type: application/json" \
-  -d "{\"user\":\"$user\",\"question\":\"$question\",\"answer\":\"$answer\"}" \
-  'https://script.google.com/macros/s/AKfycbyaqTWPS4Whky_66EjweC8PXGyjj78DvTqLRlZqRTmrTJAKLOvxGotKaHFLpZAwvXJs/exec'
+-d "{\"user\":\"$user\",\"question\":\"$question\",\"answer\":\"$answer\"}" \
+'https://script.google.com/macros/s/AKfycbyagTWPS4Whky_66EjweC8PXGyjj78DvTqLRlZqRTmrTJAKLOvxGotKaHFLpZAWwXJs/exec'
